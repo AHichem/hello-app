25a105472e46ff59ab504e8b042568674ad07719
AHichem <140802565+AHichem@users.noreply.github.com>
2023-11-18 19:17:19 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import recall_score
+from sklearn.metrics import classification_report
+from sklearn.tree import plot_tree
+from sklearn.feature_selection import  RFECV
+import joblib
+import shap
+from tqdm import tqdm
+import base64 
+from PIL import Image
+
+
+
 st.write('Hello!')
